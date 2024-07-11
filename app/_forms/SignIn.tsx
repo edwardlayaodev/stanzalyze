37f7ced9fd694e94b446501c34f2aa4b7474f728
Edward Layao <edward.layao.dev@proton.me>
2024-07-11 15:03:10 +0800

    connect sign-in and signup pages

--- app/_forms/SignIn.tsx
+++ app/_forms/SignIn.tsx
@@ -4,6 +4,7 @@ import { FormikProps, withFormik } from "formik";
 import { Atom } from "../_components/atoms";
 import { SignInValidationSchema } from "./validation/yup";
 import axios from "axios";
+import Link from "next/link";
 
 interface FormValues {
   email: string;
@@ -13,7 +14,7 @@ interface FormValues {
 const InnerForm = (props: FormikProps<FormValues>) => {
   return (
     <section className="flex flex-col gap-4">
-      <h1 className="mx-auto text-2xl font-bold">Sign-in</h1>
+      <h1 className="mx-auto text-2xl font-bold">Sign-in to your account.</h1>
       <form
         className="flex flex-col justify-center items-center gap-4"
         onSubmit={props.handleSubmit}
@@ -39,9 +40,27 @@ const InnerForm = (props: FormikProps<FormValues>) => {
           inputType="password"
         ></Atom.Input>
 
-        <Atom.Button buttonType={"btn-primary"}>Submit</Atom.Button>
+        <Atom.Button buttonType={"btn-primary"}>Sign In</Atom.Button>
         <div>{props.status}</div>
-      </form>
+      </form>{" "}
+      <div role="alert" className="alert">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="stroke-info h-6 w-6 shrink-0"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          ></path>
+        </svg>
+        <Link href="/sign-up">
+          <span className="link">Or Sign-up if you dont have an account.</span>
+        </Link>
+      </div>
     </section>
   );
 };
--- app/_forms/SignUp.tsx
+++ app/_forms/SignUp.tsx
@@ -5,6 +5,7 @@ import { FormikProps, withFormik } from "formik";
 import { Atom } from "../_components/atoms";
 import { SignUpValidationSchema } from "./validation/yup";
 import axios from "axios";
+import Link from "next/link";
 
 interface FormValues {
   email: string;
@@ -15,7 +16,9 @@ interface FormValues {
 const InnerForm = (props: FormikProps<FormValues>) => {
   return (
     <section className="flex flex-col gap-4">
-      <h1 className="mx-auto text-2xl font-bold">Sign-Up</h1>
+      <h1 className="mx-auto text-2xl font-bold">
+        Just add any email, I won't check.
+      </h1>
       <form
         className="flex flex-col justify-center items-center gap-4"
         onSubmit={props.handleSubmit}
@@ -51,9 +54,29 @@ const InnerForm = (props: FormikProps<FormValues>) => {
           inputType="password"
         ></Atom.Input>
 
-        <Atom.Button buttonType={"btn-primary"}>Submit</Atom.Button>
+        <Atom.Button buttonType={"btn-primary"}>Sign Up</Atom.Button>
         <div>{props.status}</div>
       </form>
+      <div role="alert" className="alert">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="stroke-info h-6 w-6 shrink-0"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          ></path>
+        </svg>
+        <Link href="/sign-in">
+          <span className="link">
+            Or Sign-in if you are already registered.
+          </span>
+        </Link>
+      </div>
     </section>
   );
 };
