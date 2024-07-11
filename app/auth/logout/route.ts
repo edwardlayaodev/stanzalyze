e39f249058ac4fa4da77a4a597d91408354613a1
Edward Layao <edward.layao.dev@proton.me>
2024-07-11 10:18:01 +0800

    update redirects

--- app/auth/logout/route.ts
+++ app/auth/logout/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: NextRequest) {
   }
 
   revalidatePath("/", "layout");
-  return NextResponse.redirect(new URL("/sign-in", req.url), {
+  return NextResponse.redirect(new URL("/", req.url), {
     status: 302,
   });
 }
