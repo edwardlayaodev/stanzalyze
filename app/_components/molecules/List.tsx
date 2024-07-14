c4cdc87304157ae12b61df3f01f0f9c26d5763d1
Edward Layao <edward.layao.dev@proton.me>
2024-07-14 16:51:20 +0800

    update list

--- app/_components/molecules/List.tsx
+++ app/_components/molecules/List.tsx
@@ -3,20 +3,24 @@ import Link from "next/link";
 import { Atom } from "../atoms";
 
 interface Props {
-  data: any;
+  items: any;
   header: string;
 }
 
-export default function List({ data, header }: Props) {
+export default function List({ items, header }: Props) {
   return (
     <>
       <h1 className="text-5xl font-bold text-center">{header}</h1>
       <ul className="text-left flex flex-col gap-8">
-        {data.map((item: any) => {
+        {items.map((item: any) => {
           return (
-            <Link key={item.id} href={`/dashboard/poetry/${item.id}`}>
-              <Atom.ListItem title={item.title} description={item.prompt} />
-            </Link>
+            <Atom.Collapse
+              id={item.id}
+              key={item.id}
+              title={item.title}
+              description={item.prompt}
+              content={item.result}
+            />
           );
         })}
       </ul>
